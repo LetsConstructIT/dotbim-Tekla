5d86215bc2d980334cf7f35aab7946b9b3b02571
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-11-20 20:55:44 +0100

    Class moved to separate file

--- src/dotbim.Tekla.Engine/Transformers/TeklaToDomainTransformer.cs
+++ src/dotbim.Tekla.Engine/Transformers/TeklaToDomainTransformer.cs
@@ -3,11 +3,8 @@ using dotbimTekla.Engine.Entities;
 using dotbimTekla.Engine.Exporters.Properties;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using Tekla.Structures.Geometry3d;
 using TSM = Tekla.Structures.Model;
 using TSMUI = Tekla.Structures.Model.UI;
-using TSS = Tekla.Structures.Solid;
 
 namespace dotbimTekla.Engine.Transformers;
 
@@ -42,50 +39,5 @@ public class TeklaToDomainTransformer
     public Dictionary<string, string> GetMetadata(TSM.Part part)
     {
         return new Dictionary<string, string>();
-    }
-}
-
-public class TeklaGeometryTransformer
-{
-    public Solid Transform(TSM.Part part)
-    {
-        var faces = new List<Face>();
-        var teklaSolid = part.GetSolid();
-
-        var faceEnum = teklaSolid.GetFaceEnumerator();
-        while (faceEnum.MoveNext())
-        {
-            var face = Transform(faceEnum.Current);
-            faces.Add(face);
-        }
-
-        return new Solid(faces);
-    }
-
-    private Face Transform(TSS.Face teklaFace)
-    {
-        var polygons = new List<Polygon>();
-        var loopEnum = teklaFace.GetLoopEnumerator();
-        while (loopEnum.MoveNext())
-        {
-            var loop = loopEnum.Current;
-
-            polygons.Add(Transform(loop));
-        }
-
-        return new Face(polygons.First(), teklaFace.Normal, polygons.Skip(1).ToArray());
-    }
-
-    private Polygon Transform(TSS.Loop loop)
-    {
-        var points = new List<Point>();
-
-        var vertexEnum = loop.GetVertexEnumerator();
-        while (vertexEnum.MoveNext())
-        {
-            points.Add(vertexEnum.Current);
-        }
-
-        return new Polygon(points);
     }
 }
