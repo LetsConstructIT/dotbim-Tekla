6f73ac74305f33c68bf39f0878ec361cce7b0290
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-11-23 21:55:00 +0100

    Querying model object UDAs

--- src/dotbim.Tekla.Engine/Transformers/Properties/TeklaPropertiesExporter.cs
+++ src/dotbim.Tekla.Engine/Transformers/Properties/TeklaPropertiesExporter.cs
@@ -34,28 +34,29 @@ internal class TeklaPropertiesExporter
 
         var result = new Dictionary<string, string>();
         QueryTemplate(result, modelObject, queryScope.Templates);
+        QueryUda(result, modelObject, queryScope.Udas);
 
         return result;
     }
 
-    private void QueryTemplate(Dictionary<string, string> result, ModelObject part, QueryParameters queryParameters)
+    private void QueryTemplate(Dictionary<string, string> result, ModelObject modelObject, QueryParameters queryParameters)
     {
         static void doubleQuery(ModelObject modelObject, ArrayList queryNames, Hashtable result) => modelObject.GetDoubleReportProperties(queryNames, ref result);
         static void intQuery(ModelObject modelObject, ArrayList queryNames, Hashtable result) => modelObject.GetIntegerReportProperties(queryNames, ref result);
         static void stringQuery(ModelObject modelObject, ArrayList queryNames, Hashtable result) => modelObject.GetStringReportProperties(queryNames, ref result);
 
-        SingleTypeQuery(result, part, queryParameters.DoubleNames, _convertDouble, doubleQuery);
-        SingleTypeQuery(result, part, queryParameters.IntegerNames, _convertInt, intQuery);
-        SingleTypeQuery(result, part, queryParameters.StringNames, _convertString, stringQuery);
+        SingleTypeQuery(result, modelObject, queryParameters.DoubleNames, _convertDouble, doubleQuery);
+        SingleTypeQuery(result, modelObject, queryParameters.IntegerNames, _convertInt, intQuery);
+        SingleTypeQuery(result, modelObject, queryParameters.StringNames, _convertString, stringQuery);
     }
 
-    private void SingleTypeQuery(Dictionary<string, string> result, ModelObject part, SingleTypeQuery singleTypeQuery, Func<object, string> resultConversion, Action<ModelObject, ArrayList, Hashtable> teklaQuery)
+    private void SingleTypeQuery(Dictionary<string, string> result, ModelObject modelObject, SingleTypeQuery singleTypeQuery, Func<object, string> resultConversion, Action<ModelObject, ArrayList, Hashtable> teklaQuery)
     {
         if (singleTypeQuery.QueryNames.Count == 0)
             return;
 
         var values = new Hashtable();
-        teklaQuery(part, singleTypeQuery.QueryNames, values);
+        teklaQuery(modelObject, singleTypeQuery.QueryNames, values);
 
         if (values.Count == 0)
             return;
@@ -69,7 +70,29 @@ internal class TeklaPropertiesExporter
         }
     }
 
-    private record QueryDetails(Action<ModelObject, ArrayList, Hashtable> TeklaQuery, Func<object, string> ResultFormatter);
+    private void QueryUda(Dictionary<string, string> result, ModelObject modelObject, QueryParameters udas)
+    {
+        foreach (var property in udas.DoubleNames.Properties)
+        {
+            var value = double.MinValue;
+            if (modelObject.GetUserProperty(property.TeklaName, ref value) && value != double.MinValue)
+                result[ConstructKey(property)] = _convertDouble(value);
+        }
+
+        foreach (var property in udas.IntegerNames.Properties)
+        {
+            var value = int.MinValue;
+            if (modelObject.GetUserProperty(property.TeklaName, ref value) && value != int.MinValue)
+                result[ConstructKey(property)] = _convertInt(value);
+        }
+
+        foreach (var property in udas.StringNames.Properties)
+        {
+            var value = string.Empty;
+            if (modelObject.GetUserProperty(property.TeklaName, ref value) && !string.IsNullOrEmpty(value))
+                result[ConstructKey(property)] = _convertString(value);
+        }
+    }
 
     private string ConstructKey(PropertySingle property)
         => $"{property.PSet.Name}.{property.OutputName}";
